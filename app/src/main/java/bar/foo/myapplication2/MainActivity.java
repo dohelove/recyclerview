2a06f962eb4a650a89b4866cf0894bcef11ce150
dohelove <doheepoketmon@gmail.com>
2019-10-23 15:41:59 +0900

    recyclerview best practice

--- app/src/main/java/bar/foo/myapplication2/MainActivity.java
+++ app/src/main/java/bar/foo/myapplication2/MainActivity.java
@@ -4,27 +4,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements MyAdapter.ItemViewListener {
+    List<String> dataSet;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         RecyclerView recyclerView = findViewById(R.id.recycler);
-        List<String> dataSet = generateDataSet(new String[] {"Kim", "Lee", "Won", "Park"});
-        recyclerView.setAdapter(new MyAdapter(dataSet));
+        dataSet = generateDataSet(new String[] {"Kim", "Lee", "Won", "Park"});
+        recyclerView.setAdapter(new MyAdapter(dataSet, this));
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
-
-
     }
-
 
     private List<String> generateDataSet(String[] seed) {
 
@@ -36,4 +35,8 @@ public class MainActivity extends AppCompatActivity {
         return dataSet;
     }
 
+    @Override
+    public void textViewClick(int position) {
+        Toast.makeText(this, dataSet.get(position), Toast.LENGTH_SHORT ).show();
+    }
 }
--- app/src/main/java/bar/foo/myapplication2/MyAdapter.java
+++ app/src/main/java/bar/foo/myapplication2/MyAdapter.java
@@ -2,7 +2,6 @@ package bar.foo.myapplication2;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,10 +11,12 @@ import java.util.List;
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
-    List<String> mDataSet;
+    private final ItemViewListener itemViewListener;
+    private List<String> mDataSet;
 
-    public MyAdapter(List<String> dataSet) {
+    public MyAdapter(List<String> dataSet, ItemViewListener itemViewListener) {
         this.mDataSet = dataSet;
+        this.itemViewListener = itemViewListener;
     }
 
     @NonNull
@@ -23,41 +24,39 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
         LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
         View view = inflater.inflate(R.layout.single_item, viewGroup, false);
-        MyViewHolder viewHolder = new MyViewHolder(view);
-
+        MyViewHolder viewHolder = new MyViewHolder(view, itemViewListener);
         return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder viewHolder, int i) {
         viewHolder.textView.setText(mDataSet.get(i));
-
-        Log.d("scroll test", "scrolling");
-
-        final String currentName = mDataSet.get(i);
-        viewHolder.textView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.d("[log-dohee","inside view.onclickListener");
-                Log.d("[log-dohee]","curr name: "+currentName);
-            }
-        });
     }
 
     @Override
     public int getItemCount() {
-
         return mDataSet.size();
     }
 
-    public static class MyViewHolder extends RecyclerView.ViewHolder {
+    public static class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-        public TextView textView ;
+        public TextView textView;
+        public ItemViewListener itemViewListener;
 
-        public MyViewHolder(@NonNull View itemView) {
+        public MyViewHolder(@NonNull View itemView, final ItemViewListener itemViewListener) {
             super(itemView);
+            this.itemViewListener = itemViewListener;
             textView = itemView.findViewById(R.id.textView);
-
+            itemView.setOnClickListener(this);
         }
+
+        @Override
+        public void onClick(View view) {
+            itemViewListener.textViewClick(getAdapterPosition());
+        }
+    }
+
+    public interface ItemViewListener {
+        void textViewClick(int position);
     }
 }
